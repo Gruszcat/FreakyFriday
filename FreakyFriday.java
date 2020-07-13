72b0d4215443c3e5c0e47da0d7a3a83a6f640f20
Gruszcat <gruszcat@gmail.com>
2020-07-13 21:45:22 +0000

    .replit folder created

--- FreakyFriday.java
+++ FreakyFriday.java
@@ -1,4 +1,4 @@
-public class Main{
+public class FreakyFriday{
   public static void main(String[] args){
     Person tess = new Person();
     tess.name = "Tess";
@@ -14,11 +14,12 @@ public class Main{
 
 
     System.out.println("Single mother Tess Coleman is " + tess.age + " years old and her teenage daughter Anna is " + anna.age + ".");
-    System.out.println ("\n") ;
+    System.out.println("\n");
 
     ageSwap(tess, anna);
 
     System.out.println("But after receiving cryptic fortunes at a Chinese restaurant, the two wake up the next day to discover that Tess is " + tess.age + " and Anna is " + anna.age + ". Freaky!");
+    System.out.println("\n");
   }
   private static void ageSwap(Person a, Person b){
     int c = a.age;
